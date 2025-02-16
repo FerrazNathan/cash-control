f2b233a0bde3613fd2f0eb315803a1444c7ffd95
Nathan Ferraz <nathan.ferraz@cogna.com.br>
2025-02-16 18:23:52 -0300

    Estruturas iniciais

---
+++ src/@types/styled.d.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-empty-object-type */
+import 'styled-components'
+import { defaultTheme } from '../styles/themes/themeDefault'
+
+type ThemeType = typeof defaultTheme
+
+
+declare module 'styled-components' {
+   
+  export interface DefaultTheme extends ThemeType {}
+}
