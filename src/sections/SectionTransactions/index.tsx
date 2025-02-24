96c9fff527b4eb1aaca24917a6c2176000a2375f
Nathan Ferraz <nathan.ferraz@cogna.com.br>
2025-02-24 00:04:04 -0300

    Criação de uma api com o server.json e consumindo essa api na aplicação

--- src/sections/SectionTransactions/index.tsx
+++ src/sections/SectionTransactions/index.tsx
@@ -1,43 +1,37 @@
+import { useContext } from 'react'
 import { SearchTransactions } from '../SearchTransactions'
+import { TransactionsContext } from '../../contexts/TransactionsContext';
+import { dateFormatter, priceFormatter } from '../../utils/formatter';
 
 import * as S from './styles'
 
 export function SectionTransactions() {
+  const { transactions } = useContext(TransactionsContext)
+
+console.log('TRASNACTIONS: ', transactions);
+
   return (
     <S.ContainerTransactions>
       <SearchTransactions />
       <S.TransactionsTable>
         <tbody>
-          <tr>
-            <td>Desenvolvimento de site</td>
-            <td>
-              <S.PriceHighlight variant='income'>
-                R$ 12.000,00
-              </S.PriceHighlight>
-            </td>
-            <td>Venda</td>
-            <td>17/02/2025</td>
-          </tr>
-          <tr>
-            <td>Pizza</td>
-            <td>
-              <S.PriceHighlight variant='outcome'>
-                R$ -200,00
-              </S.PriceHighlight>
-            </td>
-            <td>Alimentação</td>
-            <td>15/02/2025</td>
-          </tr>
-          <tr>
-            <td>Assaí</td>
-            <td>
-              <S.PriceHighlight variant='outcome'>
-                R$ -90,00
-              </S.PriceHighlight>
-            </td>
-            <td>Alimentação</td>
-            <td>17/02/2025</td>
-          </tr>
+          {transactions.map((transaction) => (
+            <tr key={transaction.id}>
+              <td>{transaction.description}</td>
+              <td>
+                <S.PriceHighlight variant={transaction.type}>
+                  {transaction.type === 'outcome' && '- '}
+                  {priceFormatter.format(transaction.price)}
+                </S.PriceHighlight>
+              </td>
+              <td>{transaction.category}</td>
+              <td>
+                {dateFormatter.format(
+                  new Date(transaction.createdAt)
+                )}
+              </td>
+            </tr>
+          ))}
         </tbody>
       </S.TransactionsTable>
     </S.ContainerTransactions>
